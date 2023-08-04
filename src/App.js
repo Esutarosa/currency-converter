1e956706bf7e8bc274119b61a99c26fe1ee8c17a
Esutarosa <107695658+Esutarosa@users.noreply.github.com>
2023-08-04 23:53:10 +0300

    finish block logic

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import { Block } from './Block';
 import './index.scss';
 
 function App() {
+  const [fromCurrency, setFromCurrency] = React.useState('UAH')
+  const [toCurrency, setToCurrency] = React.useState('UAH')
+  const [fromPrice, setFromPrice] = React.useState(0)
+  const [toPrice, setToPrice] = React.useState(0)
+
   const [rates, setRates] = React.useState({})
 
   React.useEffect(() => {
@@ -13,14 +18,33 @@ function App() {
         console.log(json.rates);
       })
       .catch((err) => {
-        alert('Some error')
+        alert('Some error ')
       })
   }, [])
 
+  const onChangeFromProice = (value) => {
+    setFromPrice(value)
+  }
+
+  const onChangeToProice = (value) => {
+    setToPrice(value)
+  }
+
   return (
     <div className="App">
-      <Block value={0} currency="UAH" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
+      <Block 
+        value={fromPrice} 
+        currency={fromCurrency} 
+        onChangeCurrency={setFromCurrency} 
+        onChangeValue={onChangeFromProice} 
+      />
+
+      <Block 
+        value={toPrice} 
+        currency={toCurrency} 
+        onChangeCurrency={setToCurrency} 
+        onChangeValue={onChangeToProice} 
+      />
     </div>
   );
 }
